5417437fd635f58a8fe4818763b2b71ccaed42d8
Surinderpal01 <surinderpl001@gmail.com>
2026-02-12 13:17:55 +0530

    "cors error"

--- doctor-appointment-backend/server.js
+++ doctor-appointment-backend/server.js
@@ -35,7 +35,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: ["http://localhost:5173", "http://localhost:5174",
-       "https://surinder.vercel.app","https://doc-connect-pz1doe7v2-surinders-projects-ccf2e427.vercel.app"
+       "https://docconnectapp.vercel.app","https://doc-connect-pz1doe7v2-surinders-projects-ccf2e427.vercel.app"
     ],
     credentials: true,
   })
